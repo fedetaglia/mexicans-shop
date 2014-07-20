cd30c1aa0f1ede8ea8c1424000a828368e608481
federico tagliabue <fede.tagliabue@gmail.com>
2014-07-20 10:35:14 +1000

    gemfile for heroku

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Mexican
     
     config.to_prepare do
 
-      config.assets.initialize_on_precompile = false
-
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
